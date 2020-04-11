7f2fb17d618450c2326e53a98e555908fc051984
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2020-04-11 22:25:15 +0100

    Created basic structure for return object

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,14 @@
-const covid19ImpactEstimator = (data) => data;
+import { computeImpactEstimations } from './utils';
+
+const covid19ImpactEstimator = (data) => {
+  const impact = computeImpactEstimations(data, 'normal');
+  const severeImpact = computeImpactEstimations(data, 'severe');
+
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
 
 export default covid19ImpactEstimator;
