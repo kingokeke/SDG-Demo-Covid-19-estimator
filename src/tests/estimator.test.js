94f1cb87fdc8fd0f7b38d93ab1f9f5279e004466
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2020-04-12 02:08:43 +0100

    Added tests for challenge 2

--- src/tests/estimator.test.js
+++ src/tests/estimator.test.js
@@ -49,3 +49,39 @@ test('Number of infections by requested time should double by an exponential fac
   expect(result.impact.infectionsByRequestedTime).toEqual(115200);
   expect(result.severeImpact.infectionsByRequestedTime).toEqual(576000);
 });
+
+test('Number of severe cases by request time should be 15% of number of infections by request time', () => {
+  demoData.periodType = 'month';
+  demoData.timeToElapse = 1;
+  demoData.reportedCases = 747;
+  const result = covid19ImpactEstimator(demoData);
+  expect(result.impact.severeCasesByRequestedTime).toEqual(1147392);
+  expect(result.impact.severeCasesByRequestedTime).toBeCloseTo(
+    0.15 * result.impact.infectionsByRequestedTime
+  );
+  expect(result.severeImpact.severeCasesByRequestedTime).toEqual(5736960);
+  expect(result.severeImpact.severeCasesByRequestedTime).toBeCloseTo(
+    0.15 * result.severeImpact.infectionsByRequestedTime
+  );
+});
+
+test('Number of hospital beds by request time should be the number of available hospital beds minus the number of severe cases', () => {
+  demoData.periodType = 'weeks';
+  demoData.timeToElapse = 3;
+  demoData.reportedCases = 419;
+  const result = covid19ImpactEstimator(demoData);
+  expect(result.impact.hospitalBedsByRequestedTime).toEqual(402766);
+  expect(result.impact.hospitalBedsByRequestedTime).toEqual(
+    Math.trunc(
+      result.data.totalHospitalBeds * 0.35 -
+        result.impact.severeCasesByRequestedTime
+    )
+  );
+  expect(result.severeImpact.hospitalBedsByRequestedTime).toEqual(80974);
+  expect(result.severeImpact.hospitalBedsByRequestedTime).toEqual(
+    Math.trunc(
+      result.data.totalHospitalBeds * 0.35 -
+        result.severeImpact.severeCasesByRequestedTime
+    )
+  );
+});
